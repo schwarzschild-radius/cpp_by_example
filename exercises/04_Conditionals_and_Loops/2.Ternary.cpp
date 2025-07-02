9bb92ea830ec91b3295396e89a49c5dafb8fe0c4
Vaishnavi V <vaishnavireddy8206@gmail.com>
2025-07-02 20:18:39 +0530

    Create exercise_2

---
+++ exercises/04_Conditionals_and_Loops/2.Ternary.cpp
@@ -0,0 +1,11 @@
+// This program uses a ternary operator to determine even or odd.
+
+#include <iostream>
+#include <string>
+
+int main() {
+    int number = 9;
+    std::string result;
+    result = ______ ;       //TODO: complete the code using ternary operator
+    std::cout << result << "\n";
+}
